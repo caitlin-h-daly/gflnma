ddabd1f8c79cae4b3ca301e3464fee432a46256d
Caitlin Daly <c6daly@uwaterloo.ca>
2025-06-10 17:06:51 -0400

    Made some arguments optional in plot_solution_path().

--- R/plot_solution_path.R
+++ R/plot_solution_path.R
@@ -1,15 +1,16 @@
-#' Plot the primal solution path along with critical points of interest
+#' Plot the primal solution path
 #'
 #' @description
 #' `plot_solution_path()` returns a plot of the primal solution path for objects
 #' of class `genlasso`. User must provide the `genlasso` object obtained from
-#' `solve_gflnma()` as well as a `genlasso_summary` object obtained from
-#' `genlasso_summary_table()`.
+#' `solve_gflnma()`.
 #'
 #' @details
-#' The solution path will draw vertical dashed lines at critical lambda values
-#' summarized in the `genlasso_summary` object. User may draw additional lines
-#' at specific lambda values on an ad-hoc basis via `abline()`.
+#' To draw vertical dashed lines at critical lambda values for which the
+#' parameters pool or unpool, the user must provide a `genlasso_summary` object
+#' obtained from `tabulate_solution_path()` via the `gfl_sum` argument. The user
+#' may draw additional lines at specific lambda values on an ad-hoc basis via
+#' `abline()`.
 #'
 #' The user also has the option to specify a critical region of `penfit` values
 #' through `delta_penfit_crit`. See Kong et al. (2024) for recommended rules
@@ -18,19 +19,23 @@
 #' grouped indices through `par_groups`.
 #'
 #' @param gfl_soln an object of class `genlasso`.
-#' @param gfl_sum an object of class `genlasso_summary` produced by the
-#'   `genlasso_summary_table()` function.
-#' @param lambda_scale a number indicating the desired rescaling power for the
-#'   presentation of lambda (i.e., lambda_rescaled = lambda ^ `lambda_scale`).
-#'   Default is 1.
-#' @param penfit a string indicating which penalized fit measure will be used to
-#'   select the models: one of "AIC", "AICc" (default), "BIC", "BICc".
+#' @param mod_index an optional numeric value indicating the index of the
+#'   GFL-NMA problem to plot, if multiple problems were solved by
+#'   `solve_gflnma()` (e.g.,if `length(gamma)` > 1).
+#' @param par_groups an optional list of lists of grouped indices corresponding
+#'   to sets of parameters (excluding reference) to be plotted separately
+#'   (e.g., d, b1, b2, ...).
+#' @param gfl_sum an optional object of class `genlasso_summary` produced by the
+#'   `tabulate_solution_path()` function, which is used to determine the
+#'   critical lambda values for which parameters pool or unpool.
+#' @param penfit an optional string indicating which penalized fit measure will
+#'   be used to select the models: one of "AIC", "AICc", "BIC", "BICc".
 #' @param delta_penfit_lim an optional number indicating the range of
 #'   delta_`penfit` values corresponding to models the user is willing to
 #'   consider. Default is NULL.
-#' @param par_groups an optional list of lists of grouped indicies corresponding
-#'   to sets of parameters (excluding reference) to be plotted separately
-#'   (e.g., d, b1, b2, ...).
+#' @param lambda_scale a number indicating the desired rescaling power for the
+#'   presentation of lambda (i.e., lambda_rescaled = lambda ^ `lambda_scale`).
+#'   Default is 1.
 #' @param xlimits numeric vector of length 2, giving the x coordinates range.
 #' @param ylimits numeric vector of length 2, giving the y coordinates range.
 #' @param cex_lab a numeric value indicating the expansion factor for the axis
@@ -45,21 +50,39 @@
 #' @export
 #'
 plot_solution_path <- function(gfl_soln,
-                               gfl_sum,
-                               lambda_scale = 1,
-                               penfit = "AICc",
-                               delta_penfit_lim = NULL,
+                               mod_index = NULL,
                                par_groups = NULL,
+                               gfl_sum = NULL,
+                               penfit = NULL,
+                               delta_penfit_lim = NULL,
+                               lambda_scale = 1,
                                xlimits = NULL,
                                ylimits = NULL,
                                cex_lab = 2,
                                cex_axis = 2,
                                cex_text = 0.8) {
 
+  if (length(gfl_soln) > 1) {
+    stop("Multiple GFL-NMA problems detected. Please specify which problem you
+         would like to plot the solution for via `mod_index`.")
+  }
+
+  if(!is.null(mod_index)) {
+    gfl_soln <- gfl_soln[[mod_index]]
+    if(!is.null(gfl_sum)) {
+      gfl_sum <- gfl_sum[[mod_index]]
+    }
+  } else {
+    gfl_soln <- gfl_soln[[1]]
+    if(!is.null(gfl_sum)) {
+      gfl_sum <- gfl_sum[[1]]
+    }
+  }
+
   if (!inherits(gfl_soln, "genlasso"))
     stop("\'gfl_soln\' must be an object of class `genlasso`.")
 
-  if (!inherits(gfl_sum, "genlasso_summary"))
+  if (!is.null(gfl_sum) & !inherits(gfl_sum, "genlasso_summary"))
     stop("\'gfl_sum\' must be an object of class `genlasso_summary` obtained
          from `genlasso_summary_table()`.")
 
@@ -128,13 +151,26 @@ plot_solution_path <- function(gfl_soln,
 
     # Add horizontal line for reference parameter (d_1 = b1_1 = b2_1 = ... = 0)
     abline(h = 0, col = "red")
-    # Add vertical dashed lines at every lambda where parameters pool or unpool
-    abline(v = gfl_sum$lambda ^ lambda_scale, lty = 2)
-    # Add vertical red line at lambda with smallest penfit
-    best_lambda_index <- which(gfl_sum[, paste0("delta", penfit)] == 0)
-    abline(v = gfl_sum$lambda[best_lambda_index] ^ lambda_scale,
-           col = "red",
-           lty = 2)
+
+    if(!is.null(penfit) & is.null(gfl_sum)){
+      warning("`gfl_sum` must be specified to plot vertical line at the lambda
+              with smallest penalized fit.")
+    }
+
+    if(!is.null(gfl_sum)) {
+
+      # Add vertical dashed lines at every lambda where parameters pool or unpool
+      abline(v = gfl_sum$lambda ^ lambda_scale, lty = 2)
+
+      if(!is.null(penfit)) {
+        # Add vertical red line at lambda with smallest penfit
+        best_lambda_index <- which(gfl_sum[, paste0("delta", penfit)] == 0)
+        abline(v = gfl_sum$lambda[best_lambda_index] ^ lambda_scale,
+               col = "red",
+               lty = 2)
+      }
+
+    }
 
     # Add parameter labels to lines
     if(names(par_groups)[i] == "Parameters") {
@@ -160,7 +196,7 @@ plot_solution_path <- function(gfl_soln,
 
     # Optional: Shade the region containing the lambda values that correspond to
     # models with delta_penfit within the critical region
-    if(!is.null(delta_penfit_lim)) {
+    if(!is.null(gfl_sum) & !is.null(delta_penfit_lim)) {
       # Index of models that fall within delta_penfit_lim range
       penfit_lim_index <- which(gfl_sum[, paste0("delta", penfit)] > 0 &
                                   gfl_sum[, paste0("delta", penfit)] < abs(delta_penfit_lim))
