4aecf8dce0483efc5cdba925f9a5dab71f4ea146
Caitlin Daly <c6daly@uwaterloo.ca>
2025-06-10 17:05:42 -0400

    Add GFL-NMA vignette based on Parkinson's example.

---
+++ R/dat_parkinsons.R
@@ -0,0 +1,25 @@
+#' Parkinson's NMA dataset
+#'
+#' Contrast-level data
+#'
+#' @format A data frame with 9 rows and 11 columns:
+#' \describe{
+#'   \item{`studlab`}{The study labels.}
+#'   \item{`treat1`}{The treatment code of the comparator in the observed relative
+#'   effect.}
+#'   \item{`treat2`}{The treatment code of the intervention in the observed
+#'   relative effect.}
+#'   \item{`mean1`}{.}
+#'   \item{`sd1`}{.}
+#'   \item{`n1`}{.}
+#'   \item{`mean2`}{.}
+#'   \item{`sd2`}{.}
+#'   \item{`n2`}{.}
+#'   \item{`TE`}{The observed relative treatment effects.}
+#'   \item{`seTE`}{The standard error of the observed relative treatment effects.}
+#' }
+#'
+#' @source Dias, S., Welton, N. J., Sutton, A. J., and Ades, A. E. (2011). A generalised linear modelling framework for pairwise and network meta-analysis of randomised controlled trials. In NICE DSU Technical Support Document 2. National Institute for Health and Care Excellence (NICE).
+#'
+#' data(dat_parkinsons)
+"dat_parkinsons"
